6229b4c3c36224336fbd897b30a7b9571d4cb4b3
Soumya Manna <darkprience1998@gmail.com>
2022-11-27 21:18:31 +0530

    testimonials design change

--- resources/views/frontend/testimonials.blade.php
+++ resources/views/frontend/testimonials.blade.php
@@ -6,17 +6,17 @@
             <p>{{ $description }}</p>
         </div>
         <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-            <div class="swiper-wrapper" style="height: auto">
+            <div class="swiper-wrapper mt-3" style="height: auto">
                 @if ($testimonials != '' && count($testimonials) > 0)
                     @foreach ($testimonials as $item)
                         <div class="swiper-slide">
                             <div class="testimonial-item">
-                                <p>
+                                <img src="{{ asset($item->image) }}" class="testimonial-img" alt="">
+                                <p class="mt-5">
                                     <i class="bx bxs-quote-alt-left quote-icon-left"></i>
                                     {{ $item->comment }}
                                     <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                                 </p>
-                                <img src="{{ asset($item->image) }}" class="testimonial-img" alt="">
                                 <h3>{{ $item->name }}</h3>
                                 <h4>{{ $item->profession }}</h4>
                             </div>
