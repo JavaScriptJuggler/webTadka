670d30f34fb01460ae63871fe3deab1c33d40891
Soumya Manna <darkprience1998@gmail.com>
2022-12-17 09:42:42 +0530

    engagement modal done

--- resources/views/frontend/pages/services/engagement_model.blade.php
+++ resources/views/frontend/pages/services/engagement_model.blade.php
@@ -10,15 +10,19 @@
         <div class="row">
             @if (count($serviceDetails->subservices) > 0)
                 @foreach ($serviceDetails->subservices as $item)
-                    <div class="col-lg-4 col-md-12 mb-30">
-                        <div class="model_box d-flex" style="flex-direction:column">
+                    <div class="col-lg-4 col-md-12">
+                        <div class="model_box" style="position: relative;">
                             <div class="black_bar"><img src="{{ $item->image }}">{{ $item->name }}
                             </div>
                             <div class="white_bg">
-                                <p>{{ $item->description }}
-                                    what you need. Here, expect the following:</p>
-                                {!! $item->features !!}
-                                <button class="btn btn-dark mt-auto">Book Service</button>
+                                <p class="mb-4">{{ $item->description }}
+                                    what you need. Here, expect the following:
+
+                                    {!! $item->features !!}
+                                </p>
+                                <div style="height:30px"></div>
+                                <button class="btn btn-dark p-2 m-2" style="position: absolute;right:0;bottom:0;left:0">Book
+                                    Service</button>
                             </div>
                         </div>
                     </div>
