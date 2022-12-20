189e2b0722b391e03c34d936f67d2abcbb3e7616
Soumya Manna <darkprience1998@gmail.com>
2022-12-20 17:08:02 +0530

    dual mail sendng config

---
+++ app/Mail/EnquiryMail.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class EnquiryMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $body;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('team@webtadka.com', 'Team Webtadka'),
+            subject: 'Enquiry Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'mail.admin_mails',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
