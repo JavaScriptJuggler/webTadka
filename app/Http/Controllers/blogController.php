d55daf3a0d10e19e05ac3a50abe212da714d49e9
Soumya Manna <darkprience1998@gmail.com>
2022-12-24 17:55:02 +0530

    added auth to blog controller

--- app/Http/Controllers/blogController.php
+++ app/Http/Controllers/blogController.php
@@ -11,6 +11,11 @@ use Illuminate\Support\Str;
 
 class blogController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function blogList()
     {
         $blogs = blogs::with('blogCategory')->get();
