c9083bfdcaba9a93437761c0bcfb4bc029ecf77f
Soumya Manna <darkprience1998@gmail.com>
2022-12-28 09:09:58 +0530

    show new line in about

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -2,7 +2,7 @@
     <div class="container" data-aos="fade-up">
         <div class="section-header">
             <h2>{{$about_us_header}}</h2>
-            <p>{{$about_us_description}}</p>
+            <p style="white-space: pre-line;">{{$about_us_description}}</p>
         </div>
         <div class="text-center">
             <p>{{$aboutus}}</p>
