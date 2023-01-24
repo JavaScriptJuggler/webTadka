7f9683a132c56fa61c225871c59ac32cc686f8e7
Soumya Manna <darkprience1998@gmail.com>
2023-01-24 19:19:07 +0530

    privecy policy

--- resources/views/frontend/pages/external_pages/privecy_policy.blade.php
+++ resources/views/frontend/pages/external_pages/privecy_policy.blade.php
@@ -7,24 +7,128 @@
             <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative aos-init aos-animate"
                 data-aos="zoom-out">
                 <h3>Privecy Policy</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus quisquam commodi voluptates voluptatum aperiam, incidunt corrupti atque autem maxime deserunt voluptatem magnam, facilis reprehenderit, vitae ullam ducimus a sequi?</p>
+                {{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus quisquam commodi voluptates voluptatum aperiam, incidunt corrupti atque autem maxime deserunt voluptatem magnam, facilis reprehenderit, vitae ullam ducimus a sequi?</p> --}}
             </div>
         </section>
         <!-- About section-->
         <section id="about">
             <div class="container px-2">
                 <div class="row justify-content-center">
-                    <div class="col-lg-10">
-                        <p class="lead">This is a great place to talk about your webpage. This template is purposefully
-                            unstyled so you can use it as a boilerplate or starting point for you own landing page designs!
-                            This template features:</p>
-                        <ul>
-                            <li>Clickable nav links that smooth scroll to page sections</li>
-                            <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-                            <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the
-                                navbar</li>
-                            <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+                    <div class="col-lg-12">
+                        <p class="lead">This privacy policy discloses the privacy practices for Webtadka.com. This privacy
+                            policy applies solely to information collected by this web site. It will notify you of the
+                            following:</p>
+                        <ol class="lead" type="1">
+                            <li class="mb-2"> What personally identifiable information is collected from you through the
+                                web site, how it is used and with whom it may be shared.</li>
+                            <li class="mb-2"> What choices are available to you regarding the use of your data.</li>
+                            <li class="mb-2"> The security procedures in place to protect the misuse of your information.
+                            </li>
+                            <li class="mb-2"> How you can correct any inaccuracies in the information.</li>
+                        </ol>
+                        <h3 class="display-6" style="margin-top: 2.5rem">Information Collection, Use, And Sharing</h3>
+                        <p class="lead">
+                            We are the sole owners of the information collected on this site. We only have access to/collect
+                            information that you voluntarily give us via email or other direct contact from you. We will not
+                            sell or rent this information to anyone.
+                            We will use your information to respond to you, regarding the reason you contacted us. We will
+                            not share your information with any third party outside of our organization, other than as
+                            necessary to fulfill your request, e.g. to ship an order. Unless you ask us not to, we may
+                            contact you via email in the future to tell you about specials, new products or services, or
+                            changes to this privacy policy.
+                            Your Access To And Control Over Information
+                            You may opt out of any future contacts from us at any time. You can do the following at any time
+                            by contacting us via the email address or phone number given on our website:
+                        <ul class="lead">
+                            <li>See what data we have about you, if any.</li>
+                            <li>Change/correct any data we have about you.</li>
+                            <li>Have us delete any data we have about you.</li>
+                            <li>Express any concern you have about our use of your data.</li>
                         </ul>
+                        </p>
+                        <h3 class="display-6" style="margin-top: 2.5rem">Security</h3>
+                        <p class="lead">
+                            We take precautions to protect your information. When you submit sensitive information via the
+                            website, your information is protected both online and offline. Any sensitive information (such
+                            as
+                            credit card data), collected by us is encrypted and transmitted to us in a secure way. You can
+                            verify this by looking for a closed lock icon at the bottom of your web browser, or looking for
+                            “https” at the beginning of the address of the web page. While we use encryption to protect
+                            sensitive information transmitted online, we also protect your information offline. Only
+                            employees
+                            who need the information to perform a specific job (for example, billing or customer service)
+                            are
+                            granted access to personally identifiable information. The computers/servers in which we store
+                            personally identifiable information are kept in a secure environment.
+                        </p>
+                        <h3 class="display-6" style="margin-top: 2.5rem">Updates</h3>
+                        <p class="lead">Our Privacy Policy may change from time to time and all updates will be posted on
+                            this page. If you
+                            feel that we are not abiding by this privacy policy, you should contact us immediately via
+                            email.
+                            Registration
+                            Order to use this website, a user must first complete the registration form. During registration
+                            a
+                            user is required to give certain information (such as name and email address). This information
+                            is
+                            used to contact you about the products/services on our site in which you have expressed
+                            interest. At
+                            your option, you may also provide demographic information (such as gender or age) about
+                            yourself,
+                            but it is not required.</p>
+
+                        <h3 class="display-6" style="margin-top: 2.5rem">Orders</h3>
+                        <p class="lead">We request information from you on our order form. To buy from us, you must
+                            provide contact
+                            information (like name and address) and financial information (like credit card number,
+                            expiration
+                            date). This information is used for billing purposes and to fill your orders. If we have trouble
+                            processing an order, we’ll use this information to contact you.</p>
+
+                        <h3 class="display-6" style="margin-top: 2.5rem">Cookies</h3>
+                        <p class="lead">
+                            We use “cookies” on this site. A cookie is a piece of data stored on a site visitor’s hard drive
+                            to
+                            help us improve your access to our site and identify repeat visitors to our site. For instance,
+                            when
+                            we use a cookie to identify you, you would not have to log in a password more than once, thereby
+                            saving time while on our site. Cookies can also enable us to track and target the interests of
+                            our
+                            users to enhance the experience on our site. Usage of a cookie is in no way linked to any
+                            personally
+                            identifiable information on our site. Some of our business partners may use cookies on our site
+                            (for
+                            example, advertisers). However, we have no access to or control over these cookies.
+                        </p>
+
+                        <h3 class="display-6" style="margin-top: 2.5rem">Sharing</h3>
+                        <p class="lead">
+                            We share aggregated demographic information with our partners and advertisers. This is not
+                            linked to
+                            any personal information that can identify any individual person. And/or:
+                            We use an outside shipping company to ship orders, and a credit card processing company to bill
+                            users for goods and services. These companies do not retain, share, store or use personally
+                            identifiable information for any secondary purposes beyond filling your order.
+                            And/or:
+                            We partner with another party to provide specific services. When the user signs up for these
+                            services, we will share names, or other contact information that is necessary for the third
+                            party to
+                            provide these services. These parties are not allowed to use personally identifiable information
+                            except for the purpose of providing these services.
+                        <h3 class="display-6" style="margin-top: 2.5rem">Links</h3>
+                        <p class="lead">
+                            This web site contains links to other sites. Please be aware that we are not responsible for the
+                            content or privacy practices of such other sites. We encourage our users to be aware when they
+                            leave
+                            our site and to read the privacy statements of any other site that collects personally
+                            identifiable
+                            information.
+                        </p>
+                        <h3 class="display-6" style="margin-top: 2.5rem">Contacting Us</h3>
+                        <p class="lead">
+                            For more information about our privacy practices, if you have questions, or if you would like to
+                            make a complaint, please contact us by e-mail at <a href="mailto:team@webtadka.com">team@webtadka.com</a>
+                        </p>
                     </div>
                 </div>
             </div>
