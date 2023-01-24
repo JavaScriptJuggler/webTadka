995594ddb17d5c122616d35ebeca2775ebef1c17
Soumya Manna <darkprience1998@gmail.com>
2023-01-24 19:23:08 +0530

    disclaimer

--- resources/views/frontend/pages/external_pages/disclaimer.blade.php
+++ resources/views/frontend/pages/external_pages/disclaimer.blade.php
@@ -17,16 +17,23 @@
             <div class="container px-2">
                 <div class="row justify-content-center">
                     <div class="col-lg-10">
-                        <p class="lead">This is a great place to talk about your webpage. This template is purposefully
-                            unstyled so you can use it as a boilerplate or starting point for you own landing page designs!
-                            This template features:</p>
-                        <ul>
-                            <li>Clickable nav links that smooth scroll to page sections</li>
-                            <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-                            <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the
-                                navbar</li>
-                            <li>Minimal custom CSS so you are free to explore your own unique design options</li>
-                        </ul>
+                        <p class="lead">The information contained in this website is only for general purposes. The
+                            information is provided by WebTadka.com and while we attempt to keep all the information up to
+                            date and correct, we make no representations or warranties of any type, express or implied,
+                            about the completeness, accuracy, reliability, suitability or availability with respect to the
+                            website; or the information, products, services, or related graphics contained in the website
+                            for any purpose. Any dependence you set on such information is thus strictly at your own risk.
+                            In no situation, we will be liable for any loss or damage including without limitation, indirect
+                            or resultant loss or damage; or any loss or damage whatsoever arising from loss of data or
+                            profits arise out of, or in connection with, the use of this website.
+                            Through this website you are able to link to other websites which are not under the control of
+                            WebTadka.com. We have no control over the nature, content and availability of those websites.
+                            The inclusion of any links does not necessarily indicate a recommendation or endorse the views
+                            expressed within them.
+                            Every effort is made to keep the website up and running smoothly. Yet, WebTadka.com takes no
+                            responsibility for, and will not be liable for, the website being temporarily unavailable due to
+                            technical issues beyond our control.</p>
+
                     </div>
                 </div>
             </div>
